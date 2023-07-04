de74a118cb973f4bd5b4a32e2214e0fcd33bbc8b
charuka deshan <itscharukadeshan@gmail.com>
2023-07-04 12:12:29 +0530

    add post list starter

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 /** @format */
 
 import { configureStore } from "@reduxjs/toolkit";
+import postsReducer from "../features/posts/postsSlice";
 
-export const store = configureStore({});
+export const store = configureStore({
+  reducer: {
+    post: postsReducer,
+  },
+});
---
+++ src/features/posts/postList.jsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import { UseSelector } from "react-redux/es/hooks/useSelector";
+
+import React from "react";
+
+function postList() {
+  return <div>postList</div>;
+}
+
+export default postList;
