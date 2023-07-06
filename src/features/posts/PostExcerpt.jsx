1e9516c7c57e887cc3f7c9867353e99390853576
charuka deshan <itscharukadeshan@gmail.com>
2023-07-06 19:37:41 +0530

    add view button

--- src/features/posts/PostExcerpt.jsx
+++ src/features/posts/PostExcerpt.jsx
@@ -13,10 +13,13 @@ const PostExcerpt = ({ post }) => {
       <h3 className='card-title text-black'>{post.title}</h3>
       <p className=' card-body text-gray-900'>{post.body.substring(0, 75)}</p>
       <div>
-        <Link to={`post/${post.id}`}>View Post</Link>
-
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
+        <div className='my-4'>
+          <Link to={`post/${post.id}`}>
+            <button className='btn btn-outline btn-sm'>View Post</button>
+          </Link>
+        </div>
         <ReactionButtons post={post} />
       </div>
     </article>
